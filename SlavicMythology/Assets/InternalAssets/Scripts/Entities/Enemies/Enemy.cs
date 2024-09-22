57d216cd3361b6420dc05b1c7fb569f7791762ad
MrFixMan007 <schurkin.danil@yandex.ru>
2024-09-22 17:03:02 +0400

    Добавил, обход врагами препятствий, изменены слои

--- SlavicMythology/Assets/InternalAssets/Scripts/Entities/Enemies/Enemy.cs
+++ SlavicMythology/Assets/InternalAssets/Scripts/Entities/Enemies/Enemy.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
+using Pathfinding;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
 public class Enemy : MonoBehaviour
 {
     public float detectionRadius = 5f;
@@ -10,38 +13,63 @@ public class Enemy : MonoBehaviour
     public int damage = 10;
     public float attackCooldown = 1f;
 
-    private Transform _player;
+    private Transform _target;
     private bool _canAttack = true;
-    private Rigidbody2D _rigidBody2D;
+    private Rigidbody2D _rb;
     private Vector2 movement;
+
+    [SerializeField] private float _nextWaypointDistance = 3f;
+    private Path _path;
+    private int _currentWayPoint = 0;
+
+    private Seeker _seeker;
 
     private void OnValidate()
     {
-        _rigidBody2D ??= GetComponent<Rigidbody2D>();
+        _rb ??= GetComponent<Rigidbody2D>();
+        _seeker ??= GetComponent<Seeker>();
     }
 
     private void Awake()
     {
-        _rigidBody2D ??= GetComponent<Rigidbody2D>();
+        _rb ??= GetComponent<Rigidbody2D>();
+        _seeker ??= GetComponent<Seeker>();
     }
 
     void Start()
     {
-        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        InvokeRepeating(nameof(UpdatePath), 0f, 0.5f);
+    }
+
+    private void UpdatePath()
+    {
+        if (_seeker.IsDone())
+            _seeker.StartPath(_rb.position, _target.position, OnPathComplete);
+    }
+
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            _path = p;
+            _currentWayPoint = 0;
+        }
     }
 
     void Update()
     {
-        if (_player != null)
+        if (_path != null)
         {
-            Vector3 direction = _player.position - transform.position;
+            Vector3 direction = _target.position - transform.position;
             float distanceToPlayer = direction.magnitude;
-            movement = direction.normalized;
-            
+
             if (distanceToPlayer <= detectionRadius)
             {
                 // Преследуем игрока
-                MoveChar(movement);
+                MoveChar();
 
                 if (distanceToPlayer <= attackRadius && _canAttack)
                 {
@@ -50,10 +78,18 @@ public class Enemy : MonoBehaviour
             }
         }
     }
-    
-    private void MoveChar(Vector2 direction)
+
+    private void MoveChar()
     {
-        _rigidBody2D.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        Vector2 direction = ((Vector2)_path.vectorPath[_currentWayPoint] - _rb.position).normalized;
+        Vector2 movePosition = (Vector2)transform.position + (direction * moveSpeed * Time.deltaTime);
+        _rb.MovePosition(movePosition);
+
+        float distance = Vector2.Distance(_rb.position, _path.vectorPath[_currentWayPoint]);
+        if (distance < _nextWaypointDistance && _currentWayPoint < _path.vectorPath.Count - 1)
+        {
+            _currentWayPoint++;
+        }
     }
 
     private IEnumerator Attack()
@@ -61,7 +97,7 @@ public class Enemy : MonoBehaviour
         _canAttack = false;
 
         // Логика нанесения урона
-        if (_player.TryGetComponent(out Health playerHealth))
+        if (_target.TryGetComponent(out Health playerHealth))
         {
             playerHealth.TakeDamage(damage);
         }
